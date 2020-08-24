5ea1d678dee1620747299e65a8ef3489dc9db606
Rohun Vora <emailrohun@gmail.com>
2020-08-23 20:33:25 -0700

    [main.js] add copy invite link funciton

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,15 @@ var socket = io();
 window.onload = function () {
   socket.emit("hello", "Viva La Vida");
 };
+
+let copyButton = document.querySelector('#copyButton')
+
+copyButton.addEventListener('click', (event => {
+  let inviteLink = document.createElement('input'), text = window.location.href;
+
+  document.body.appendChild(inviteLink);
+  inviteLink.value = text;
+  inviteLink.select();
+  document.execCommand('copy');
+  document.body.removeChild(inviteLink)
+}))
